8d4f026bd157e087aa57bdbb371da7b08f4e1b4d
mark <marknery@ddaniel-win7.den.er.corp>
2013-01-11 16:47:18 -0700

    baselined with start-kit

---
+++ elpa/idle-highlight-mode-1.1.2/idle-highlight-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; idle-highlight-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (idle-highlight-mode) "idle-highlight-mode" "idle-highlight-mode.el"
+;;;;;;  (20720 42043))
+;;; Generated autoloads from idle-highlight-mode.el
+
+(autoload 'idle-highlight-mode "idle-highlight-mode" "\
+Idle-Highlight Minor Mode
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("idle-highlight-mode-pkg.el") (20720 42043
+;;;;;;  830942))
+
+;;;***
+
+(provide 'idle-highlight-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; idle-highlight-mode-autoloads.el ends here
