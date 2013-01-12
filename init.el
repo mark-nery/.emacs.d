d9944bb42d1c732d596c45963e5585152f8ec9ff
mark <marknery@marks-MacBook-Pro.local>
2013-01-11 21:01:53 -0700

    updated theme

--- init.el
+++ init.el
@@ -6,9 +6,11 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby starter-kit-js)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby starter-kit-js twilight-theme)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+(load-theme 'twilight t)
